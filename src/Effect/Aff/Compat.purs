a2f1049c6511ce49eaed24774a72390eb12129c3
Eric Brisco <ebrisco@corumdigital.com>
2019-09-06 14:06:42 -0400

    FIX: imported wrong Error

--- src/Effect/Aff/Compat.purs
+++ src/Effect/Aff/Compat.purs
@@ -6,10 +6,9 @@ module Effect.Aff.Compat
   , module Exports
   ) where
 
-import Effect.Aff (Aff)
+import Effect.Aff (Aff, Error)
 import Effect.Aff.General.Compat (EffectFnCb) as Exports
 import Effect.Aff.General.Compat as G
-import Effect.Exception (Error)
 import Effect.Uncurried (EffectFn1, EffectFn2, EffectFn3, mkEffectFn1, mkEffectFn2, mkEffectFn3, runEffectFn1, runEffectFn2, runEffectFn3)
 import Prelude (type (~>))
 
